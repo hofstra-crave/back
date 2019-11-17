93ce4a2a0188371f739a38e091082f924a4e737e
Jeremy Robinson <jrobinson7@pride.hofstra.edu>
2019-11-17 17:08:23 -0500

    connected db

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 const Koa = require('koa');
 const static_server = require('koa-static');
 const Router = require('koa-router');
+const mariadb = require('mariadb');
+require('dotenv').config();
+
+const pool = mariadb.createPool({
+  host: process.env.HOST_NAME,
+  user: process.env.USER_NAME,
+  password: process.env.PASSWORD,
+  connectionLimit: 5
+});
+
+pool.getConnection().then(() => {
+  console.log('Connected to the db');
+});
 
 const app = new Koa();
 let router = new Router();
