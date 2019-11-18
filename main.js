846ce178006333c248783303364138bd6d7d8855
Jeremy Robinson <jrobinson7@pride.hofstra.edu>
2019-11-18 18:52:37 -0500

    added http server

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 const static_server = require('koa-static');
 const Router = require('koa-router');
 const mariadb = require('mariadb');
+const http = require('http');
 require('dotenv').config();
 
 const pool = mariadb.createPool({
@@ -20,38 +21,44 @@ let router = new Router();
 
 app.use(static_server('./public'));
 
-// Get Restaurant 
-router.get("/getRestaurant/:restaurantName", async (context, next) => {
+// Get Restaurant
+router.get('/getRestaurant/:restaurantName', async (context, next) => {
   let conn;
   try {
     conn = await pool.getConnection();
-    const response = await conn.query(`SELECT Restaurant FROM Restaurants WHERE RestaurantName = ${context.params.restaurantName}`)
+    const response = await conn.query(
+      `SELECT Restaurant FROM Restaurants WHERE RestaurantName = ${context.params.restaurantName}`
+    );
     const data = await response.json();
     context.response.body = data;
   } catch (err) {
     throw err;
   } finally {
-    if (conn) conn.end()
+    if (conn) conn.end();
   }
-})
+});
 
-router.get("/getRestaurant/:id", async (context, next) => {
+router.get('/getRestaurant/:id', async (context, next) => {
   let conn;
   try {
     conn = await pool.getConnection();
-    const response = await conn.query(`SELECT Restaurant FROM Restaurants WHERE RestaurantID = ${context.params.id}`)
+    const response = await conn.query(
+      `SELECT Restaurant FROM Restaurants WHERE RestaurantID = ${context.params.id}`
+    );
     const data = await response.json();
     context.response.body = data;
   } catch (err) {
     throw err;
   } finally {
-    if (conn) conn.end()
+    if (conn) conn.end();
   }
-})
+});
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.listen(3000, () => {
-  console.log('Server is up and running');
+const httpServer = http.createServer(app.callback());
+
+httpServer.listen(80, () => {
+  console.log('HTTP Server running on port 80');
 });
